6fed72b5f011d14f62a71bf6e11438210649d6f1
Lujain Alamil <lojalameel@gmail.com>
2026-03-03 23:39:21 +0300

    Lab 4

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    name = request.GET.get("name") or "world!"
+    return render(request, "bookmodule/index.html")
+
+def index2(request, val1 = 0):   
+    return HttpResponse("value1 = "+str(val1))
+
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+ 
